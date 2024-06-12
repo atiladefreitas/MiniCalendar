0c95cfba14d789061ac3e85220641d3464e73fc7
Átila de Freitas <atila.defreitas@icloud.com>
2024-06-12 10:10:42 -0300

    Initial Commit

---
+++ MiniCalendar/MiniCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniCalendarApp.swift
+//  MiniCalendar
+//
+//  Created by Átila de Freitas on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MiniCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
